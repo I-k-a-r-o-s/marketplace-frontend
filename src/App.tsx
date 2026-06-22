2ea39a374a605ef0b5de5358a1f7a7daeab12578
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-22 16:08:40 +0530

    placeholder pages and added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
