9febc60b81b2f48b0c4e7d7ccaee792b6c386f45
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-06-29 12:05:52 +0530

    check user validity upon refreshes and logs in if neecessary

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,39 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Navbar from "./components/Navbar";
 import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
+import api from "./api/api";
+import { useDispatch, useSelector } from "react-redux";
+import { signInSuccess, signOut } from "./redux/user/userSlice";
+import type { RootState } from "./redux/store";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  const { currentUser } = useSelector((state: RootState) => state.user);
+
+  useEffect(() => {
+    if (currentUser) return;
+
+    const validate = async () => {
+      try {
+        const { data } = await api.get("/api/auth/validate");
+        if (data.success && data.userData) {
+          dispatch(signInSuccess(data.userData));
+        } else {
+          dispatch(signOut());
+        }
+      } catch (error) {
+        dispatch(signOut());
+        console.log("Error in validate!:", error);
+      }
+    };
+    validate();
+  }, [dispatch, currentUser]);
   return (
     <>
-    <Toaster position="bottom-center" reverseOrder={false} />
-    <Navbar />
+      <Toaster position="bottom-center" reverseOrder={false} />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
