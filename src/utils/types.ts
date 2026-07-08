efcc37c410e1e0d3fa7f9d83020f421d72170e5d
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-07-08 19:39:52 +0530

    util for storing types

---
+++ src/utils/types.ts
@@ -0,0 +1,41 @@
+export type AuthModeType = "signin" | "signup";
+
+export type UpdateListingModalProps = {
+  listing: ListingType;
+  onUpdated: () => Promise<void>;
+  disabled: boolean;
+};
+
+export type ListingType = {
+  _id: string;
+  name: string;
+  description: string;
+  address: string;
+  price: number;
+  discountedPrice?: number;
+  bathrooms: number;
+  bedrooms: number;
+  furnished?: boolean;
+  parking?: boolean;
+  typeOfPlace: "rent" | "sell";
+  offer: boolean;
+  images: string[];
+  cloudinaryImagePublicIds?: string[];
+  createdAt: string;
+};
+
+
+export type FormDataType = {
+  name: string;
+  description: string;
+  address: string;
+  typeOfPlace: "sell" | "rent";
+  parking: boolean;
+  furnished: boolean;
+  offer: boolean;
+  bedrooms: number;
+  bathrooms: number;
+  price: number;
+  discountedPrice: number;
+  images: File[];
+};
