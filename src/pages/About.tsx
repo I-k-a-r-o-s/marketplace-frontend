dca0d1c3cc16e0f2cfaafe83d634494305ca4963
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-07-17 11:37:41 +0530

    placeholer about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,11 @@
-
 const About = () => {
   return (
-    <div><title>About</title>About</div>
-  )
-}
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <title>About</title>About
+      <h1 className="text-3xl font-bold mb-5 text-primary">About Estates</h1>
+      <p className="mb-4 ">placeholder for about description</p>
+    </div>
+  );
+};
 
-export default About
+export default About;
